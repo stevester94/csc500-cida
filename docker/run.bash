f219846ea8c729a56cfe5913235954a74b02904a
Steven Mackey <stevester94@gmail.com>
2021-06-12 17:03:32 -0700

    Add DANN submod

--- docker/run.bash
+++ docker/run.bash
@@ -1,8 +1,10 @@
 #! /bin/bash
 #-u $(id -u):$(id -g) \
 docker run \
+--gpus=all \
 --network=host  \
 -ti \
 --rm  \
 -v $CSC500_ROOT_PATH:/csc500-super-repo  \
+--shm-size=16g \
 csc500-cida-torch
